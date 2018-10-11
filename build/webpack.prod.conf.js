8ac246555aa0b67ea618c6bdf5a7b7950427726e
Антон Степаненков <Lenseg1@gmail.com>
2018-10-12 02:12:44 +0300

    kek

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -12,14 +12,10 @@ const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const gitCommitHash = utils.getCommitHash();
 
-const env =
-  process.env.NODE_ENV === 'testing'
-    ? require('../config/test.env')
-    : require('../config/prod.env');
-const buildConf =
-  process.env.NODE_ENV === 'testing'
-    ? require('../config/build.test')
-    : require('../config/build.prod');
+const isTesting = process.env.NODE_ENV === 'testing';
+const env = isTesting
+  ? require('../config/test.env')
+  : require('../config/prod.env');
 const webpackConfig = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({
@@ -28,7 +24,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       usePostCSS: true,
     }),
   },
-  entry: buildConf.entry,
+  entry: isTesting ? config.test.entry : config.build.entry,
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
   output: {
     path: config.build.assetsRoot,
@@ -74,7 +70,7 @@ const webpackConfig = merge(baseWebpackConfig, {
         process.env.NODE_ENV === 'testing' ? 'index.html' : config.build.index,
       template: 'index.html',
       inject: true,
-      chunks: buildConf.chunks,
+      chunks: isTesting ? config.test.chunks : config.build.chunks,
       chunksSortMode: 'manual',
       minify: {
         removeComments: true,
